abadf5f964eed94e260f02d259a130f924c6c56c
Mxolisi <ngwenyaharris34@gmail.com>
2021-10-05 13:54:05 +0200

    Updated userHandler.

--- Controllers/UserController.py
+++ Controllers/UserController.py
@@ -1,10 +1,12 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 from flask import jsonify
 import os
 import time
+import jwt
 from .LogsHandler import Logs
 from ..DBF.DBConnect import Connect
 from ..DBF.DBFunctions import DBFunctions
+from ..Functions.config import Config
 
 class UserHandler (object):
     def __init__(self):
@@ -24,13 +26,22 @@ class UserHandler (object):
             Logs.recordLogs(f"LoginException: {str(exc)}")
     
     @staticmethod
-    def registerUser(request):
+    def registerUser(request): #CALL spRegisterUser(0817302568, 561212472084, "John Doe", "john@email.com", "John@doe123"); 
         try:
             dbfObj = DBFunctions()
             phone_number = request.json['contact_number']
             id_number = request.json['id_number']
-            Logs.recordLogs(f"registerUser: phone_number {phone_number}, id_number {id_number}")
-            dbfObj.RegisterUser(phone_number, id_number)
-            return jsonify({"Success": True, "messages": ["Registration Success", "You will recieve an sms confirming your registration shortly."], "Token": res[0]})
+            fullname = request.json['fullname']
+            email = request.json['email']
+            password = request.json['password']
+
+            Logs.recordLogs(f"registerUser data: {phone_number} {id_number} {fullname} {email} {password}")
+            res = dbfObj.RegisterUser(phone_number, id_number, fullname, email, password)
+            jwt_token = jwt.encode({
+                    'user_token': res[0],
+                    'sessions_expires_on': str(datetime.utcnow() + timedelta(hours = 8))
+                },
+                Config['secret_key'])
+            return jsonify({"Success": True, "messages": ["Registration Success", "You will recieve an sms confirming your registration shortly."], "auth_token": jwt_token})
         except Exception as exc:
             Logs.recordLogs(f"RegistrationException: {str(exc)}")
